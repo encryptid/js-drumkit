7e235d8b9a23890e2a7cba0ab573a6ba95e83e3d
Joshua Lawson <jacklegjosh@gmail.com>
2017-03-06 16:49:13 -0500

    sounds connected

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ window.addEventListener('keydown', function(ev) {
      * references an audio element with a "data-key" of whatever the key code of the button that was pressed. It
      * feels a bit hacky to me, however it's also very clever.
      */
-    console.log(snip);
+    //console.log(audio);
+    if (!snip) return; // if it is not a "snip", return (or terminate)
+    snip.currentTime = 0; // each keydown sets the current snip time to zero or "rewinds" it, so that the sounds
+        // can be overlaid
+    snip.play(); //play the audio item
 })
