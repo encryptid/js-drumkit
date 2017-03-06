47e5dc84e64d174daa0e4720aff1a19f73df5ad5
Joshua Lawson <jacklegjosh@gmail.com>
2017-03-06 16:36:39 -0500

    reference audio items

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 window.addEventListener('keydown', function(ev) {
-    console.log(ev);
+    const snip = document.querySelector(`audio[data-key="${ev.keyCode}"]`);
+    /** This is really interesting; so what we're doing here is, upon keydown, we're creating a query selector that
+     * references an audio element with a "data-key" of whatever the key code of the button that was pressed. It
+     * feels a bit hacky to me, however it's also very clever.
+     */
+    console.log(snip);
 })
